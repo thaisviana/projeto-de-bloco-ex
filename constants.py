d29fdd3e9136e46bca1eb2bad027c5236114fd42
Thais <thaisviana@users.noreply.github.com>
2019-09-02 09:44:25 -0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+
+branco = (255,255,255)
+preto = (0,0,0)
+red = (255,0,0)
+darkBlue = (2, 24, 89)
+
+largura_tela, altura_tela = 800, 600
+
+dicionario = {'alexandre': 456123789, 'anderson': 1245698456,
+              'antonio': 123456456, 'carlos': 91257581,
+              'cesar':987458, 'rosemary': 789456125 }
