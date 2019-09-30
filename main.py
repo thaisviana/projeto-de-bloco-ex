80a4b1f1a4a29bbea99c39481d50e0713dd62405
Thais <thaisviana@users.noreply.github.com>
2019-09-30 09:25:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame, random
 from aba import Aba
+import os
+import time
 from constants import (branco, preto, red, darkBlue, largura_tela,
 altura_tela, infos)
 
@@ -8,21 +10,22 @@ pygame.font.init()
 terminou= False
 #Dimensões da tela
 tela = pygame.display.set_mode((largura_tela, altura_tela))
-
 soma_rss = sum([i["rss"] for i in infos])/1024/1024/1024
 soma_vms = sum([i["vms"] for i in infos])/1024/1024/1024
 soma = 0
 soma_media = 0
+titulo = ["ARQUIVOS", "PROCESSOS", "REDE 1", "REDE 2"]
 
-def mostra_titulo(texto, x, y, fonte=20):
-      font = pygame.font.Font(None, fonte)
+
+def mostra_titulo(texto, x, y, tamanho_fonte=18):
+      font = pygame.font.SysFont("impact", tamanho_fonte)
       text = font.render(texto, 1, preto)
       textpos = text.get_rect(center=(x, y))
       textpos.left = x
       tela.blit(text, textpos)
 
 def mostra_titulo_aba(texto, x):
-      font = pygame.font.Font(None, 24)
+      font = pygame.font.SysFont("nirmalaui", 24)
       text = font.render(texto, 1, branco)
       textpos = text.get_rect(center=(x, 30))
       tela.blit(text, textpos)
@@ -30,11 +33,14 @@ def mostra_titulo_aba(texto, x):
 def formata_valores(soma):
     soma_indices = 1
     for item in infos:
+          vms = item["vms"]/1024/1024
+          rss = item["vms"]/1024/1024
           mostra_titulo(f'{item["pid"]}',100, 160 + soma_indices*22)
           mostra_titulo(f'{item["nome"]}', 160, 160 + soma_indices*22)
-          mostra_titulo(f'{round(item["vms"]/1024/1024,2)}MB',400, 160 + soma_indices*22)
-          mostra_titulo(f'{round(item["rss"]/1024/1024,2)}MB',500, 160 + soma_indices*22)
-          mostra_titulo(f'{item["percento"]}',600, 160 + soma_indices*22)
+          mostra_titulo(f'{round(vms,2)} MB',400, 160 + soma_indices*22)
+          mostra_titulo(f'{round(rss,2)} MB',500, 160 + soma_indices*22)
+          mostra_titulo(f'{round((vms/(soma_vms*1024))*100,2)} %',620, 160 + soma_indices*22)
+          mostra_titulo(f'{round((rss/(soma_rss*1024))*100,2)} %',700, 160 + soma_indices*22)
           soma_indices = soma_indices + 1
 
 def cria_abas():
@@ -44,17 +50,35 @@ def cria_abas():
         aba = Aba(i, darkBlue)
         aba.desenha(tela)
         lista_de_abas.append(aba)
-        mostra_titulo_aba(f"ABA {i}", (largura_tela/4 * aba_type)+100)
+        mostra_titulo_aba(f"{titulo[i]}", (largura_tela/4 * aba_type)+100)
     return lista_de_abas
 
 def mostra_conteudo_aba_0(conta_segundos):
-    mostra_titulo(f'ABA 0 - {conta_segundos}s',100, 100, 24)
-    mostra_titulo("ACME Inc.           Uso do espaço em disco pelos processos",100,130)
+    mostra_titulo(f'ARQUIVOS - {conta_segundos}s',100, 100, 24)
+    mostra_titulo(f'index',40,160)
+    mostra_titulo(f'Nome Arquivo',100,160)
+    mostra_titulo(f'Tamanho',240,160)
+    mostra_titulo(f'Data Modificação',340,160)
+    mostra_titulo(f'Data Criação',590,160)
+    lista = os.listdir()
+    soma_indices = 1
+    for i in lista: 
+        if os.path.isfile(i):
+            mostra_titulo(f'{soma_indices}',40, 160 + soma_indices*22)
+            mostra_titulo(f'{i}', 100, 160 + soma_indices*22)
+            mostra_titulo(f'{round(os.stat(i).st_size/1024,2) }KB',240, 160 + soma_indices*22)
+            mostra_titulo(f'{time.ctime(os.stat(i).st_mtime)}',340, 160 + soma_indices*22)
+            mostra_titulo(f'{time.ctime(os.stat(i).st_atime)}',590, 160 + soma_indices*22)
+            soma_indices += 1
+
+def mostra_conteudo_aba_1(conta_segundos):
+    mostra_titulo(f'PROCESSOS - {conta_segundos}s',100, 100, 24)
     mostra_titulo(f'PID',100,160)
     mostra_titulo(f'NOME',160,160)
     mostra_titulo(f'VMS',400,160)
     mostra_titulo(f'RSS',500,160) 
-    mostra_titulo(f'% do uso',600,160) 
+    mostra_titulo(f'% VMS',620,160)
+    mostra_titulo(f'% RSS',700,160) 
     formata_valores(soma)
     mostra_titulo(f"Total de memoria RSS usada: {round(soma_rss,2)} Gb",100,870)
     mostra_titulo(f"Total de memoria VMS usada: {round(soma_vms,2)} Gb",100,850)
@@ -85,7 +109,7 @@ while not terminou:
              if aba1.area.collidepoint(pos):
                  init_abas()
                  aba_selecionada = 1
-                 mostra_titulo( f'ABA 1 | {conta_segundos}s',100, 300, 24)
+                 mostra_conteudo_aba_1(conta_segundos)
              if aba2.area.collidepoint(pos):
                  aba_selecionada = 2
                  init_abas()
@@ -108,7 +132,7 @@ while not terminou:
           if aba_selecionada == 0:
               mostra_conteudo_aba_0(conta_segundos)
           elif aba_selecionada == 1:
-              mostra_titulo(f'ABA 1 | {conta_segundos}s',100, 300, 24)
+              mostra_conteudo_aba_1(conta_segundos)
           elif aba_selecionada == 2:
               mostra_titulo(f'ABA 2| {conta_segundos}s',100, 300, 24)
           elif aba_selecionada == 3:
